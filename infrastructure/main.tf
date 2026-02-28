72d9f5b490b13d098beba0abcc4d51997d026ea9
Tobias Boyer <tobias.boyer@outlook.com>
2026-02-28 14:08:42 +0100

    Restore hcloud_token variable definition in variables.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -24,12 +24,6 @@ terraform {
   }
 }
 
-# Set the variable value in *.tfvars file
-# or using the -var="hcloud_token=..." CLI option
-variable "hcloud_token" {
-  sensitive = true
-}
-
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
   token = var.hcloud_token
